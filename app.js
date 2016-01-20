2f253126d814092f353b6968c0c33aeb2421aa6c
emiliooo <emilburaczynski@gmail.com>
2016-01-20 18:53:08 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+    function CheckingForm(){
+       var text=$('.box').find('textarea#tekst');
+       var formName=$('.box').find('span.formName');
+
+
+        if(text.val().length>0)
+        {
+            formName.hide();
+        }
+
+        else{
+            formName.show();
+
+        }
+    }
+
+    $('button').on('click', function(){
+        CheckingForm();
+
+    });
+
+
+
+
+
+});
